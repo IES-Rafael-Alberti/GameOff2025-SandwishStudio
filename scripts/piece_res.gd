b6a7d048558b34ffe1bae097791c641278637b44
Jusm8 <migacota@gmail.com>
2025-11-12 16:16:48 +0100

    Arreglo para que ejecute

--- scripts/piece_res.gd
+++ scripts/piece_res.gd
@@ -1,11 +1,23 @@
 extends Resource
-
 class_name PieceRes
 
-enum Race { NORDICA, JAPONESA, EUROPEA }
-enum Tier { COMUN, RARO, EPICO, LEGENDARIO }
+# Nombres únicos para evitar choque con otros enums/vars
+enum PieceRace { NORDICA, JAPONESA, EUROPEA }
+enum PieceTier { COMUN, INUSUAL, RARO, EPICO }
 
 @export var id: String = ""
 @export var display_name: String = ""
-@export var Race: Race 
-@export var Tier: Tier
+@export var race: PieceRace = PieceRace.NORDICA
+@export var tier: PieceTier = PieceTier.COMUN
+@export var role: String = ""
+
+@export_range(1, 20, 1) var members_per_piece: int = 6
+
+@export var frames: SpriteFrames
+@export var max_health: float = 100.0
+@export var health: float = 100.0
+@export var damage: float = 10.0
+@export var attack_speed: float = 1.0
+@export var critical_chance: int = 5
+@export var critical_damage: float = 1.5
+@export var gold_per_enemy: int = 0
