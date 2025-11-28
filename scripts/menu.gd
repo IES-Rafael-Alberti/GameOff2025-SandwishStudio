dc4d01d9ce06f75d4b482c8f191ac955be6b87e8
Jusm8 <migacota@gmail.com>
2025-11-28 19:48:06 +0100

    Gran avance menu de play sus opciones

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,21 +1,32 @@
 extends Control
 
-# Es una buena práctica cargar la escena de opciones una sola vez
-# Así no tiene que leerla del disco cada vez que se pulsa el botón
-var options_scene = preload("res://scenes/options.tscn")
-
+var options_scene: PackedScene = preload("res://scenes/options.tscn")
+var options_instance: Control = null
+var is_animating: bool = false
 
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
-
 func _on_options_pressed() -> void:
-	var options_instance = options_scene.instantiate()
+	if is_animating:
+		return
+
+	if is_instance_valid(options_instance):
+		is_animating = true
+		options_instance.close_with_anim()
+		return
+
+	options_instance = options_scene.instantiate()
+
+	options_instance.opened.connect(_on_options_opened)
+	options_instance.closed.connect(_on_options_closed)
 
 	add_child(options_instance)
-	
-	get_tree().paused = true
+	is_animating = true
 
+func _on_options_opened() -> void:
+	is_animating = false
 
-func _on_quit_pressed() -> void:
-	get_tree().quit()
+func _on_options_closed() -> void:
+	is_animating = false
+	options_instance = null
--- scripts/options.gd
+++ scripts/options.gd
@@ -1,12 +1,43 @@
 extends Control
 
-func _on_back_pressed() -> void:
-	get_tree().paused = false
-	queue_free()
+signal opened
+signal closed
 
-func _on_exit_pressed() -> void:
-	pass
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+@onready var papiro: TextureRect = $Papiro
+
+var _visible_pos: Vector2
+var _hidden_pos: Vector2
+var _closing: bool = false
+
+func _ready() -> void:
+	_visible_pos = papiro.position
+	_hidden_pos = Vector2(-papiro.size.x - 350.0, _visible_pos.y)
+	papiro.position = _hidden_pos
+
+	var tween := create_tween()
+	tween.tween_property(papiro, "position", _visible_pos, 0.45) \
+		.set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
+	# cuando termina de entrar, avisamos al menú
+	tween.tween_callback(func():
+		opened.emit()
+	)
+
+func close_with_anim() -> void:
+	if _closing:
+		return
+	_closing = true
+
+	var tween := create_tween()
+	tween.tween_property(papiro, "position", _hidden_pos, 0.35) \
+		.set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_IN)
+	tween.tween_callback(func():
+		closed.emit()
+		queue_free()
+	)
+
+func _on_back_pressed() -> void:
+	close_with_anim()
+
+func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("pause"):
-		_on_back_pressed()
+		close_with_anim()
